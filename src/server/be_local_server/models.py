43b4a82febb33dec74cedf48d45a63ff0eac1101
scottlow <slow@uvic.ca>
2014-09-28 10:31:43 -0700

    Made some modifications to the data model.
    Changed most DateFields to DateTimeFields to include time
    
    Fixed a typo in the name ProductTag
    
    Changed the addr_type field to an enum, as I think this is what we
    want? Feel free to correct me if I misunderstood the purpose of this
    field.
    
    Removed the GeoLocation class, as we were essentially just mapping each
    Geolocation to an address, so I figured we might as well just lump lat
    and long straight into the address entity. This should also simplify
    the creation of addresses on the client side.
    
    Made Vendor inherit from django.contrib.auth.models.User so that it
    inherits all the data fields that are already being stored in the
    database.
    
    Removed the stock attribute from Product, as I think it makes more
    sense to have stock as an attribute on the SellerProductAtLocation
    relation.

--- src/server/be_local_server/models.py
+++ src/server/be_local_server/models.py
@@ -2,71 +2,66 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-	name = models.CharField(max_length=200)
-	description = models.CharField(max_length=400)
-	price = models.IntegerField()
-	stock = models.CharField(max_length=300)
-	image_path = models.CharField(max_length=300)
-	created_at = models.DateField()
-	updated_at = models.DateField()
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    price = models.IntegerField()
+    image_path = models.CharField(max_length=300)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
 class Tag(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
-class ProdcutTag(models.Model):
-	product = models.ManyToManyField(Product)
-	tag = models.ForeignKey(Tag)
-	created_at = models.DateField()
-	updated_at = models.DateField()
+class ProductTag(models.Model):
+    product = models.ManyToManyField(Product)
+    tag = models.ForeignKey(Tag)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
 class Address(models.Model):
-	addr_line1 = models.CharField(max_length=400)
-	addr_line2 = models.CharField(max_length=400)
-	city = models.CharField(max_length=200)
-	state = models.CharField(max_length=200)
-	country = models.CharField(max_length=200)
-	zipcode = models.CharField(max_length=10)
-	addr_type = models.CharField(max_length=100)
-	updated_at = models.DateField()
+    MARKET = 'MAR'
+    FARM = 'FAR'
+    ADDR_TYPES = (
+        (MARKET, 'Market'),
+        (FARM, 'Farm'),
+    )
 
-class GeoLocation(models.Model):
-	latitude = models.IntegerField();
-	longitude = models.IntegerField();
+    addr_type = models.CharField(max_length=3, choices=ADDR_TYPES, default=FARM)
 
-class Vendor(models.Model):
-	company_name = models.CharField(max_length=200)
-	webpage = models.CharField(max_length=400)
-	image_path = models.CharField(max_length=300)
-	is_approved = models.BooleanField(default=False)
-	register_date = models.DateField()
+    addr_line1 = models.CharField(max_length=400)
+    addr_line2 = models.CharField(max_length=400)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=10)
 
-class VendorContact(models.Model):
-	vendor = models.ForeignKey(Vendor)
-	first_name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
-	country_code = models.CharField(max_length=50)
-	phone = models.CharField(max_length=25)
-	extension = models.CharField(max_length=25)
-	email = models.CharField(max_length=200)
-	contact_type = models.CharField(max_length=200)
-	created_at = models.DateField()
-	updated_at = models.DateField()
+    latitude = models.FloatField();
+    longitude = models.FloatField();
+
+    created_at = models.DateTimeField()    
+    updated_at = models.DateTimeField()
+
+class Vendor(User):
+    company_name = models.CharField(max_length=200)
+    webpage = models.CharField(max_length=400)
+    country_code = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    extension = models.CharField(max_length=25)
 
 class SellerLocation(models.Model):
-	vendor = models.ForeignKey(Vendor)
-	geo_location = models.ForeignKey(GeoLocation)
-	address = models.ForeignKey(Address)
-	start_time = models.DateField()
-	end_time = models.DateField()
-	name = models.CharField(max_length=200)
-	image_path = models.CharField(max_length=300)
-	created_at = models.DateField()
-	updated_at = models.DateField()
+    vendor = models.ForeignKey(Vendor)
+    address = models.ForeignKey(Address)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    image_path = models.CharField(max_length=300)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
 class SellerProductAtLocation(models.Model):
-	sellerLocation = models.ForeignKey(SellerLocation)
-	product = models.ForeignKey(Product)
-	is_visible = models.BooleanField(default=False)
-	created_at = models.DateField()
-	updated_at = models.DateField()
-	stock = models.CharField(max_length=200)
+    sellerLocation = models.ForeignKey(SellerLocation)
+    product = models.ForeignKey(Product)
+    is_visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    stock = models.CharField(max_length=100)

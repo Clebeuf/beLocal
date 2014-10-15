57b93d60e212f034ddad60f0e51c1bf84245e2ce
Carly Lebeuf <clebeuf@uvic.ca>
2014-10-15 16:59:28 -0700

    Edited sellering locations UI

--- src/client/app/scripts/controllers/details.js
+++ src/client/app/scripts/controllers/details.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('DetailsCtrl', function ($scope, StateService) {
+  .controller('DetailsCtrl', function ($scope, $timeout, StateService) {
 
     StateService.getTrendingProducts().then(function() {
       $scope.trendingProducts = StateService.getTrendingProductsList();
@@ -14,6 +14,14 @@ angular.module('clientApp')
     StateService.getVendorInfo().then(function() {
     	$scope.vendorDetails = StateService.getVendorDetails();
     });
+
+    $timeout(function() {
+		var container = document.querySelector('#masonry-container');
+		var msnry = new Masonry(container, {
+		  itemSelector: '.ms-item',
+		  columnWidth: '.ms-item'
+		});    
+    }, 1000)
 
   });
 

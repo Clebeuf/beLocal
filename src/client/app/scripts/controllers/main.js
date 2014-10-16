8049f56218f4103032a39bb9281b5f39c1e25b7a
CakeBrewery <cakebrewery@gmail.com>
2014-10-15 18:29:52 -0700

    changed market card layout and added temporary mockup data

--- src/client/app/scripts/controllers/main.js
+++ src/client/app/scripts/controllers/main.js
@@ -11,8 +11,43 @@ angular.module('clientApp')
     	$scope.vendors = StateService.getVendorsList();
     })
 
+/* Uncomment this for actual data (non-hardcoded)
     StateService.getMarkets().then(function() {
     	$scope.marketlist = StateService.getMarketList();
+      console.log($scope.marketlist);
     })
+
+*/
+
+    //This is hardcoded data
+    StateService.getMarkets().then(function() {
+      $scope.marketlist = [
+        {
+          "name":"The Nice Market",
+          "addr_line1":"9079 Interesting Ave.",
+          "addr_type":"MAR",
+          "city":"Victoria",
+          "country":"Canada",
+          "state":"BC",
+          "zipcode":"5p69i3",
+          "running_info":"Monday to Friday",
+          "hours":"9:00am to 3:00pm"
+        },
+
+        {
+          "name":"Local Marketplace",
+          "addr_line1":"9760 Douglas St.",
+          "addr_type":"MAR",
+          "city":"Saanich",
+          "country":"Canada",
+          "state":"BC",
+          "zipcode":"9i91r3",
+          "running_info":"Every Wednesday",
+          "hours":"9:00am to 4:00pm"
+        }
+
+      ]
+    })
+
 
   });

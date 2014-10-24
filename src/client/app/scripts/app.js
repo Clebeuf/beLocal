63bae687a78a59debc73a9fae11e4dfcbec2b270
scottlow <slow@uvic.ca>
2014-10-23 22:31:18 -0700

    Started work on welcome view
    The biggest change here is that CSS styles are no longer defined in
    index.html. Instead, they are defined on a per-state basis in app.js.
    This was due to the fact that there were a large number of (necessary)
    conflicting styles between main.css and welcome.css.

--- src/client/app/scripts/app.js
+++ src/client/app/scripts/app.js
@@ -13,23 +13,33 @@ var app = angular.module('clientApp', [
 ])
   .config(function ($stateProvider, $httpProvider, $urlRouterProvider) {
     $stateProvider
+    .state('welcome', {
+      url: '/welcome',
+      templateUrl: 'views/welcome.html',
+      controller: 'WelcomeCtrl',
+      authenticate: false,
+      css: 'styles/welcome.css'
+    })    
     .state('main', {
       url: '/',
       templateUrl: 'views/main.html',
       controller: 'MainCtrl',
-      authenticate: false
+      authenticate: false,
+      css: 'styles/main.css'      
     })
     .state('seller', {
       url: '/seller',
       templateUrl: 'views/seller.html',
       controller: 'SellerCtrl',
-      authenticate: true
+      authenticate: true,
+      css: 'styles/main.css' 
     })
     .state('vendor/details', {
       url: '/vendor/details/{vendorid:[0-9]{1,8}}',
       templateUrl: 'views/details.html',
       controller: 'DetailsCtrl',
-      authenticate: false
+      authenticate: false,
+      css: 'styles/main.css'
     });
 
 
@@ -75,3 +85,34 @@ app.directive('holderFix', function () {
         }
     };
 });
+
+app.directive('head', ['$rootScope','$compile',
+    function($rootScope, $compile){
+        return {
+            restrict: 'E',
+            link: function(scope, elem){
+                var html = '<link rel="stylesheet" ng-repeat="(stateCtrl, cssUrl) in stateStyles" ng-href="{{cssUrl}}" />';
+                elem.append($compile(html)(scope));
+                scope.stateStyles = {};
+                $rootScope.$on('$stateChangeStart', function (e, next, nextParams, current) {
+                    if(current && current.css){
+                        if(!Array.isArray(current.css)){
+                            current.css = [current.css];
+                        }
+                        angular.forEach(current.css, function(sheet){
+                            delete scope.stateStyles[sheet];
+                        });
+                    }
+                    if(next && next.css){
+                        if(!Array.isArray(next.css)){
+                            next.css = [next.css];
+                        }
+                        angular.forEach(next.css, function(sheet){
+                            scope.stateStyles[sheet] = sheet;
+                        });
+                    }
+                });
+            }
+        };
+    }
+]);
---
+++ src/client/app/scripts/controllers/welcome.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('clientApp')
+  .controller('WelcomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

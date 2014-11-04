eb2377cc958968c80fc06090ffd9e129b8ac437d
scottlow <slow@uvic.ca>
2014-11-03 21:58:33 -0800

    Removed debug statements and made empty search results pretty

--- src/client/app/scripts/controllers/search.js
+++ src/client/app/scripts/controllers/search.js
@@ -21,16 +21,11 @@ angular.module('clientApp')
 
         $scope.doSearch = function() {
             if($stateParams.search_type === 'products') {
-                console.log('Product searching!');
                 $scope.doProductSearch($stateParams.q);
             } else if($stateParams.search_type === 'vendors') {
-                console.log('Vendor searching!');
                 $scope.doVendorSearch($stateParams.q);            
             } else if($stateParams.search_type === 'markets') {
-                console.log('Market searching!');
                 $scope.doMarketSearch($stateParams.q);            
-            } else {
-                console.log('Error!');
             }
         } 
 
--- src/client/app/scripts/controllers/typeahead.js
+++ src/client/app/scripts/controllers/typeahead.js
@@ -8,7 +8,7 @@
  * Controller of the clientApp
  */
 var app = angular.module('clientApp')
-app.controller('TypeaheadCtrl', function ($scope, $http, StateService, $sce) {
+app.controller('TypeaheadCtrl', function ($scope, $http, StateService, $sce, $window) {
 	$scope.productSuggestions = [];
   	$scope.updateProductSuggestions = function(val) {
   		return $http.get(StateService.getServerAddress() + "search/autocomplete?q=" + val
@@ -24,9 +24,9 @@ app.controller('TypeaheadCtrl', function ($scope, $http, StateService, $sce) {
   	$scope.onSelect = function($item,$model,$label){
 
   		if($item.vendorSearch != null) {
-  			window.location.href='#/search/vendors?q=' + $item.vendorSearch;
+  			$window.location.href='#/search/vendors?q=' + $item.vendorSearch;
   		} else {
-  			window.location.href='#/search/products?q=' + $item.name;
+  			$window.location.href='#/search/products?q=' + $item.name;
   		}
   	}
   });

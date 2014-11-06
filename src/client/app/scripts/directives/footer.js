645b5d177e6cbc88fe56777ca4c2837e8fee9978
Carly Lebeuf <clebeuf@uvic.ca>
2014-11-06 11:32:12 -0800

    created new footer directive & added it to all the pages

---
+++ src/client/app/scripts/directives/footer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name clientApp.directive:footer
+ * @description
+ * # footer
+ */
+angular.module('clientApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'scripts/directives/footer.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+
+
+      }
+    };
+  });

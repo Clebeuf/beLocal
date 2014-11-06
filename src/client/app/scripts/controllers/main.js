cffac9007668f152d7e687a4299628e4ae3fcbc1
sheoranjs24 <sheoranjs24@gmail.com>
2014-11-05 16:19:28 -0800

    use angularjs filter for category and tags.

--- src/client/app/scripts/controllers/main.js
+++ src/client/app/scripts/controllers/main.js
@@ -6,6 +6,7 @@ angular.module('clientApp')
     $scope.showCategory = false;
     $scope.showTag = false;
     $scope.selectedCategory = 'All Products';
+    $scope.selectedTag = 'All Products';
     
     $scope.displayVendor = function (id) {
       $location.path('vendor/details/'+id).replace();
@@ -81,8 +82,6 @@ angular.module('clientApp')
   $scope.marketMasonry();
   
   $scope.resetProductLists = function() {
-    $scope.selectedCategoryProducts = undefined;
-    $scope.selectedTagProducts = undefined;
     $scope.showCategory = false;
     $scope.showTag = false;
   }
@@ -91,22 +90,18 @@ angular.module('clientApp')
     $scope.resetProductLists();
     $scope.showCategory = true;
     $scope.selectedCategory = category.name;
-    StateService.getSelectedCategoryProducts(category).then(function() {
-      $scope.selectedCategoryProducts = StateService.getSelectedCategoryProductsList();
-    });
   }
   
   $scope.getProductsWithTag = function(tag) {
     $scope.resetProductLists();
     $scope.showTag = true;
-    StateService.getSelectedTagProducts(tag).then(function() {
-      $scope.selectedTagProducts = StateService.getSelectedTagProductsList();
-    });
+    $scope.selectedTag = tag.name;
   }
   
   $scope.getAllProducts = function() {  
     $scope.resetProductLists();
     $scope.selectedCategory = 'All Products';
+    $scope.selectedTag = 'All Products';
   }
 
 });

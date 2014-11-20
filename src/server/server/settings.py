9f8556906619b1b05a7bcd51b7673d6df072dcd1
CakeBrewery <cakebrewery@gmail.com>
2014-11-19 16:21:36 -0800

    changed settings.py to use mysql

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -100,8 +100,7 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
     }
 }
 

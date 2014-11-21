534994bf3edf70bf036fc7dade54f4213431f43b
Carly Lebeuf <clebeuf@uvic.ca>
2014-11-21 00:31:36 -0800

    Touched up the about us page

--- src/client/app/scripts/controllers/contactus.js
+++ src/client/app/scripts/controllers/contactus.js
@@ -1,17 +1,47 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name clientApp.controller:ContactusCtrl
- * @description
- * # ContactusCtrl
- * Controller of the clientApp
- */
 angular.module('clientApp')
-  .controller('ContactusCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactusCtrl', function ($scope, $location, $timeout, StateService, $q, $rootScope, $window) {
+    $scope.showXSNav = false;
+    $scope.emailAddress = "belocal" + "victoria" + "@gmail.com";
+
+
+    $scope.safeApply = function(fn) {
+      var phase = this.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        if(fn && (typeof(fn) === 'function')) {
+          fn();
+        }
+      } else {
+        this.$apply(fn);
+      }
+    };
+
+	$scope.checkNav = function() {
+      if(angular.element($window).scrollTop() > 140 || angular.element($window).width() < 768) {
+        $scope.safeApply(function() {
+          $scope.showXSNav = true;
+        });
+      } else {
+        $scope.safeApply(function() {
+          $scope.showXSNav = false;
+        })
+      }        
+    }
+
+    if(angular.element($window).width() < 768) {
+      $scope.safeApply(function() {
+        $scope.showXSNav = true;
+      });
+    }            
+
+    angular.element($window).resize(function() {
+        $scope.checkNav();
+    });    
+
+    angular.element($window).scroll(function(){
+        $scope.checkNav();        
+    });
+
+
   });

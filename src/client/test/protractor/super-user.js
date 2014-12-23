7148cdabd3b6393a108089626a4c0d9868044486
scottlow <slow@uvic.ca>
2014-12-22 20:48:21 -0800

    Adding one more timeout to wait for modal to open

--- src/client/test/protractor/super-user.js
+++ src/client/test/protractor/super-user.js
@@ -7,10 +7,12 @@ describe('The beLocal Farmer Splash Page', function() {
     it('Should allow superuser sign in without Facebook', function() {
 
         // Let's test the login button in the nav-bar!
-        element(by.css('[ng-click="getStarted()"]')).click();
+        element(by.css('[ng-click="getStarted()"]')).click();       
         browser.sleep(1000);
 
         element(by.css('[ng-click="clearLoginModal()"]')).click();
+        browser.sleep(1000);
+        
         element(by.model('loginUsername')).clear().sendKeys('superuser');
         element(by.model('loginPassword')).clear().sendKeys('belocal');
         element(by.css('[ng-click="loginSubmit()"]')).click();

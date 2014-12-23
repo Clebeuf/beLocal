7c6cabbb903fba7c1df1b04b473036ba225bb422
Carly Lebeuf <clebeuf@uvic.ca>
2014-12-22 21:11:10 -0800

    merged in changes from protractor branch

--- src/client/app/scripts/controllers/welcome.js
+++ src/client/app/scripts/controllers/welcome.js
@@ -2,7 +2,7 @@
 
 angular.module('clientApp')
   .controller('WelcomeCtrl', function ($scope, AuthService, StateService, $location, ipCookie, $timeout, $http, $filter) {
-  	$scope.AuthService = AuthService;
+    $scope.AuthService = AuthService;
 
     // Scroll to a DOM element with a specific id
     // 1250 represents the duration of the animation
@@ -14,14 +14,13 @@ angular.module('clientApp')
 
     // Check to see if there is a hashtag in the url when coming to this page. If so, it means we have to extract it and scroll
     // to a certain part of the page. (This is how the register dropdown works when you're not signed in)
-  	var url = document.location.toString();
-  	if (url.split('#')[2]) {
+    var url = document.location.toString();
+    if (url.split('#')[2]) {
         $timeout(function(){
-  	      $scope.scrollTo('#' + url.split('#')[2]);
+          $scope.scrollTo('#' + url.split('#')[2]);
           $location.hash('');
         }, 250);
-  	}
-
+    }
 
     $scope.sendTemplateEmail = function (params){
 
@@ -164,23 +163,28 @@ angular.module('clientApp')
 
     // Try signing up as a customer with Facebook. If there is already an account associated with the currently authenticated Facebook
     // account, a 304 will be returned from the server, prompting an error message to be displayed.
-  	$scope.signUpAsCustomer = function() {
-  		AuthService.createCustomer().then(function(status) {
-  			if(status === 304) {
+    $scope.signUpAsCustomer = function() {
+      AuthService.createCustomer().then(function(status) {
+        if(status === 304) {
           $scope.accountAlreadyCreated = true;
-  			}
-  		});
-  	}
+        } else {
+          angular.element('#createUserModal').modal('hide');         
+        }
+      });
+    }
 
     // Try signing up as a vendor with Facebook. If there is already an account associated with the currently authenticated Facebook
     // account, a 304 will be returned from the server, prompting an error message to be displayed.
-  	$scope.signUpAsVendor = function() {
-  		AuthService.createVendor().then(function(status) {
-  			if(status === 304) {
+    $scope.signUpAsVendor = function() {
+      AuthService.createVendor().then(function(status) {
+        if(status === 304) {
           $scope.accountAlreadyCreated = true;          
-  			}
-  		});
-  	}
+        } else {
+          console.log(StateService.getCurrentUser());
+          angular.element('#createUserModal').modal('hide');        
+        }
+      });
+    }
 
     // Set a flag to sign up as a vendor (this changes the appearance of some modals in the HTML)
     $scope.signUpAsVendorNoFB = function() {
@@ -195,11 +199,10 @@ angular.module('clientApp')
     }
 
     // If a user presses the "Start Browsing Now" button, we set a cookie to hide the splash page next time they visit beLocal
-  	$scope.getStarted = function() {
-  		ipCookie('beLocalBypass', true, {expires: 14});  		
-  		$location.path('/');
-  	}  
-
+    $scope.getStarted = function() {
+      ipCookie('beLocalBypass', true, {expires: 14});     
+      $location.path('/');
+    }  
 
     // Create a new vendor without Facebook
     $scope.newVendorSubmit = function() {
@@ -229,7 +232,7 @@ angular.module('clientApp')
           $scope.emailErrorMessage = response.email;
         }                  
       });
-    }; 
+    } 
 
     // Create a new customer without Facebook
     $scope.newCustomerSubmit = function() {
@@ -247,8 +250,8 @@ angular.module('clientApp')
       }
       AuthService.createNonFacebookCustomer(data)
       .success(function() {
-        angular.element('#createUserModal').modal('hide');  
-        $scope.foodieSendEmail();
+        angular.element('#createUserModal').modal('hide');
+        $scope.foodieSendEmail();  
       })
       .error(function(response) {
         // Set a flag to sign up as a vendor (this changes the appearance of some modals in the HTML)
@@ -258,7 +261,7 @@ angular.module('clientApp')
           $scope.emailErrorMessage = response.email;
         }          
       });   
-    }; 
+    } 
 
     // When the create user modal is hidden, check to see what kind of user we have just created, and redirect to the appropriate page in beLocal.
     angular.element('#createUserModal').on('hidden.bs.modal', function(e) {
@@ -269,7 +272,10 @@ angular.module('clientApp')
                 $location.path('/');                    
               });
           } else if(StateService.getUserType() === 'VEN') {
+              console.log(StateService.getCurrentUser());
+              console.log("hit");
               $timeout(function() {
+                console.log('please work!!!!!!');
                 $location.path('/vendor');
               });
           }

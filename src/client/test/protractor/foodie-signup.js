9d69dd6449adc4c05d52e276f3d83482aec0b132
scottlow <slow@uvic.ca>
2014-12-27 20:47:58 -0800

    Added checks for foodie sign up
    We’re now checking the following cases:
    1. Foodie tries to login with a Facebook account that has no beLocal
    account associated with it
    2. Foodie tries to sign up with a Facebook account that already has a
    beLocal account associated with it
    3. Foodie tries to register with a username that already exists
    4. Foodie tries to register with an email that already exists

--- src/client/test/protractor/foodie-signup.js
+++ src/client/test/protractor/foodie-signup.js
@@ -21,13 +21,13 @@ describe('Sign Up as a Foodie With and Without Facebook', function() {
         browser.sleep(1000);
         browser.waitForAngular();
         expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/');
-    });      
+    });
 
-    it('Should allow sign up as a foodie with Facebook', function() {
-        element(by.css('[ng-click="signUpAsCustomerNoFB()"]')).click();
+    it('Should prevent login with a Facebook account that does not have a beLocal account associated with it', function() {
+        element(by.css('[ng-click="clearLoginModal()"]')).click();
         browser.sleep(1000);
 
-        element(by.css('[ng-click="signUpAsCustomer()"]')).click();
+        element(by.css('[ng-click="signIn()"]')).click();
 
         //Wait for Facebook pop-up to load
         //We cannot use waitForAngular() here since Facebook popup is not angular
@@ -51,9 +51,31 @@ describe('Sign Up as a Foodie With and Without Facebook', function() {
 
             //Switch back to main window (is this even necessary?)
             browser.driver.switchTo().window(handles[0]);
-       		browser.waitForAngular();
-        	expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/');
+            browser.waitForAngular();
+            expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/welcome');
+            expect(element(by.css('[ng-show="noFacebookAccountError"]')).isDisplayed()).toBeTruthy();
         });        
+    });          
+
+    // This works since the Facebook cookie is never cleared despite our call to deleteAllCookies()
+    it('Should allow sign up as a foodie with Facebook', function() {
+        element(by.css('[ng-click="signUpAsCustomerNoFB()"]')).click();
+        browser.sleep(1000);
+
+        element(by.css('[ng-click="signUpAsCustomer()"]')).click();
+        browser.sleep(2000);        
+       	browser.waitForAngular();
+        expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/');
+    });
+
+    it('Should prevent sign up as a foodie with a already used Facebook account', function() {
+        element(by.css('[ng-click="signUpAsCustomerNoFB()"]')).click();
+        browser.sleep(1000);
+
+        element(by.css('[ng-click="signUpAsCustomer()"]')).click();
+        browser.sleep(1000);        
+        expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/welcome');
+        expect(element(by.css('[ng-show="accountAlreadyCreated"]')).isDisplayed()).toBeTruthy();
     });
 
     it('Should allow sign up as a foodie without Facebook', function() {
@@ -71,5 +93,41 @@ describe('Sign Up as a Foodie With and Without Facebook', function() {
         browser.sleep(1000);
         browser.waitForAngular();
         expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/');
-    });   
+    });
+
+    it('Should prevent sign up as a foodie without Facebook when an username that already exists is entered', function() {
+        element(by.css('[ng-click="signUpAsCustomerNoFB()"]')).click();
+        browser.sleep(1000);
+
+        element(by.model('newVendorFirstName')).clear().sendKeys('Test');
+        element(by.model('newVendorLastName')).clear().sendKeys('Foodie');
+        element(by.model('newVendorUserName')).clear().sendKeys('testfoodie');
+        element(by.model('newVendorEmail')).clear().sendKeys('testfoodie1@belocalvictoria.me');
+        element(by.model('newVendorPassword')).clear().sendKeys('belocal');
+
+        element(by.css('[ng-click="newCustomerSubmit()"]')).click();
+
+        browser.sleep(1000);
+        browser.waitForAngular();
+        expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/welcome');
+        expect(element(by.css('[ng-show="usernameErrorMessage != null"]')).isDisplayed()).toBeTruthy();
+    });
+
+    it('Should prevent sign up as a foodie without Facebook when an email that already exists is entered', function() {
+        element(by.css('[ng-click="signUpAsCustomerNoFB()"]')).click();
+        browser.sleep(1000);
+
+        element(by.model('newVendorFirstName')).clear().sendKeys('Test');
+        element(by.model('newVendorLastName')).clear().sendKeys('Foodie');
+        element(by.model('newVendorUserName')).clear().sendKeys('testfoodie1');
+        element(by.model('newVendorEmail')).clear().sendKeys('testfoodie@belocalvictoria.me');
+        element(by.model('newVendorPassword')).clear().sendKeys('belocal');
+
+        element(by.css('[ng-click="newCustomerSubmit()"]')).click();
+
+        browser.sleep(1000);
+        browser.waitForAngular();
+        expect(browser.getCurrentUrl()).toEqual('http://127.0.0.1:9000/#/welcome');
+        expect(element(by.css('[ng-show="emailErrorMessage != null"]')).isDisplayed()).toBeTruthy();
+    });            
 });

d2c137c1e2836a7f1630ec6ea70c690f3bcd6656
scottlow <slow@uvic.ca>
2015-03-02 01:10:07 -0800

    Missed edge case in serializers

--- src/server/be_local_server/serializers.py
+++ src/server/be_local_server/serializers.py
@@ -189,7 +189,7 @@ class SellerLocationSerializer(serializers.ModelSerializer):
         today = datetime.combine(date.today(), datetime.min.time())
 
         # Make sure that the recurrence we get back always has valid days
-        if(obj.real_start and obj.address.hours.all().count() > 0 and obj.real_start.weekday() + 1 > obj.address.hours.all()[obj.address.hours.all().count() - 1].weekday):
+        if(obj.real_start and obj.address.hours.all().count() > 0 and date.today() < obj.real_start and obj.real_start.weekday() + 1 > obj.address.hours.all()[obj.address.hours.all().count() - 1].weekday):
           next = obj.recurrences.after(today + timedelta(days=today.weekday(), weeks=1), inc=True)
         else:
           next = obj.recurrences.after(today, inc=True)      
@@ -215,7 +215,7 @@ class ListSellerLocationSerializer(serializers.ModelSerializer):
         today = datetime.combine(date.today(), datetime.min.time())
 
         # Make sure that the recurrence we get back always has valid days
-        if(obj.real_start and obj.address.hours.all().count() > 0 and obj.real_start.weekday() + 1 > obj.address.hours.all()[obj.address.hours.all().count() - 1].weekday):
+        if(obj.real_start and obj.address.hours.all().count() > 0 and date.today() < obj.real_start and obj.real_start.weekday() + 1 > obj.address.hours.all()[obj.address.hours.all().count() - 1].weekday):
           next = obj.recurrences.after(today + timedelta(days=today.weekday(), weeks=1), inc=True)
         else:
           next = obj.recurrences.after(today, inc=True)     
